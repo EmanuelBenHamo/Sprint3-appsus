dbd59aa3b26cb9782e738452e95b50bd984faaf8
Emanuel Ben Hamo <embenh@gmail.com>
2020-02-26 13:13:34 +0200

    added render of notes in keep app cmp

--- js/pages/keep-app.cmp.js
+++ js/pages/keep-app.cmp.js
@@ -1,9 +1,29 @@
-// 'use strict';
+import {keepService} from '../apps/keep/services/keep.service.js';
 
-export default{
+export default {
     template:`
     <section class='keep-app-container'>
         <h1>Keep App!</h1>
+        <section class="notes-container" v-if="notes">
+            <component v-for="note in notes" :is="note.type"></component>
+        </section>
     </section>
-    `
+    `,
+    data(){
+        return {
+            notes: null
+        }
+    },
+    created(){
+        keepService.getNotes()
+        .then(notes => this.notes = notes);
+    },
+    components: {
+        'note-text': noteText,
+        'note-img': noteImg,
+        'note-todos': noteTodos,
+        'note-video': noteVideo,
+        'note-audio': noteAudio,
+        'note-map': noteMap
+    }
 }
