5f242f047de0494102507f5548777c33a7c9394b
Emanuel Ben Hamo <embenh@gmail.com>
2020-02-26 15:45:05 +0200

    fix misatke in notes service

--- js/apps/keep/services/keep.service.js
+++ js/apps/keep/services/keep.service.js
@@ -5,7 +5,7 @@ import { utilService } from '../../../services/utils-service.js';
 const NOTES_KEY = 'notes';
 var notesDB = [];
 
-function getNotes(){
+function getNotes() {
     let notes = localStorage.getItem(NOTES_KEY);
     
     if(!notes) {
@@ -21,26 +21,26 @@ function getNotes(){
 function addNote(note) {
     notesDB.push(note);
     utilService.store(NOTES_KEY, notes);
-    return Promise.resolve(notesDB);
+    return Promise.resolve(note);
 }
 
 function deleteNote(noteId){
     let noteIdx = notesDB.findIndex(note => note.id === noteId);
     let deletedNote = notesDB.splice(noteIdx, 1)[0];
     utilService.store(NOTES_KEY, notesDB);
-    return Promise.resolve(notesDB);
+    return Promise.resolve(deletedNote);
 }
 
 function updateNote(updatedNote) {
     let noteIdx = notesDB.findIndex(note => note.id === updatedNote.id);
     notesDB.splice(noteIdx, 1, updatedNote);
     utilService.store(NOTES_KEY, notesDB);
-    return Promise.resolve(notesDB);
+    return Promise.resolve(updatedNote);
 }
 
 export const keepService = {
     getNotes,
     addNote,
-    updatedNote,
-    deletedNote
+    updateNote,
+    deleteNote
 } 
