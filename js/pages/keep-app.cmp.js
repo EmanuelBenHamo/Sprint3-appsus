8b16b0c2aefc10876dc729dd88df76fd1c41d0c8
Emanuel Ben Hamo <embenh@gmail.com>
2020-02-26 20:54:52 +0200

    added note filter cmp

--- js/pages/keep-app.cmp.js
+++ js/pages/keep-app.cmp.js
@@ -1,11 +1,13 @@
 import {keepService} from '../apps/keep/services/keep.service.js';
-import noteList from '../apps/keep/cmps/note-list.cmp.js';
+import noteFilter from '../apps/keep/cmps/note-filter.cmp.js';
 import noteInput from '../apps/keep/cmps/note-input.cmp.js';
+import noteList from '../apps/keep/cmps/note-list.cmp.js';
 
 export default {
     template:`
     <section class='keep-app-container'>
         <h1>Keep App!</h1>
+        <note-filter @filtered="setFilter"></note-filter>
         <note-input @addedNote="onAddedNote"></note-input>
         <section class="pinnedNotes">
             <h2>Pinned</h2>
@@ -19,7 +21,8 @@ export default {
     `,
     data(){
         return {
-            notes: null
+            notes: null,
+            filterBy: null
         }
     },
     computed:{
@@ -43,10 +46,14 @@ export default {
         onAddedNote(note) {
             keepService.addNote(note)
             .then(addedNote => console.log('note added', JSON.stringify(addedNote)));
+        },
+        setFilter(filterBy) {
+            this.filterBy = filterBy;
         }
     },
     components: {
-        noteList,
-        noteInput
+        noteInput,
+        noteFilter,
+        noteList
     },
 }
