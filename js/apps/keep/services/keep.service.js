82c998d8095baabf81668ac0e576ddc1e0306ec5
Emanuel Ben Hamo <embenh@gmail.com>
2020-02-26 15:10:02 +0200

    moved notes fake data array to outer file that imported from service

--- js/apps/keep/services/keep.service.js
+++ js/apps/keep/services/keep.service.js
@@ -1,37 +1,11 @@
 'use strict';
 
-const keepFakeData = [
-    {
-        id: 1001,
-        type: 'noteText',
-        info: {
-            txt: 'My Text is amazing'
-        }
-    },
-    {
-        id: 1002,
-        type: 'noteImg',
-        info: {
-            url: 'https://api.adorable.io/avatars/285/abott@adorable.png',
-            title: 'My pretty face'
-        }
-    },
-    {
-        id: 1003,
-        type: 'noteTodos',
-        info: {
-            todos: [
-                { txt: 'Clean house', doneAt: null },
-                { txt: 'Clean house', doneAt: 187111111 },
-            ]
-        }
-    },      
-]
+import {fakeNotes} from './notes-fake-data.js';
 
 function getNotes(){
-    return Promise.resolve(keepFakeData);
+    return Promise.resolve(fakeNotes);
 }
 
 export const keepService = {
-    getNotes,
+    getNotes
 } 
---
+++ js/apps/keep/services/notes-fake-data.js
@@ -0,0 +1,27 @@
+export const fakeNotes = [
+    {
+        id: 1001,
+        type: 'noteText',
+        info: {
+            txt: 'My Text is amazing'
+        }
+    },
+    {
+        id: 1002,
+        type: 'noteImg',
+        info: {
+            url: 'https://api.adorable.io/avatars/285/abott@adorable.png',
+            title: 'My pretty face'
+        }
+    },
+    {
+        id: 1003,
+        type: 'noteTodos',
+        info: {
+            todos: [
+                { txt: 'Clean house', doneAt: null },
+                { txt: 'Clean house', doneAt: 187111111 },
+            ]
+        }
+    },      
+]
