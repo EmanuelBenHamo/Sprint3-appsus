4524e8efb23d16f96771fbdf55de0900f7a7b4c9
Emanuel Ben Hamo <embenh@gmail.com>
2020-02-26 20:19:35 +0200

    fixed bug in input cmp - moved cmp type keys to data

--- js/apps/keep/cmps/note-input.cmp.js
+++ js/apps/keep/cmps/note-input.cmp.js
@@ -1,44 +1,43 @@
-import {keepService} from '../services/keep.service.js';
-
-const noteTypes = keepService.NOTE_TYPES;
+import { keepService } from '../services/keep.service.js';
 
 export default {
     template: `
         <section class="note-input-container">
             <input type="text" :placeholder="inputPlaceholder" v-model="input" @change="onAddedNote">
             <section class="note-type-option-btns-container">
-                <button class="note-type-option-btn note-type-txt-btn" @click="inputType='noteTypes.text'">text</button>
-                <button class="note-type-option-btn note-type-img-btn" @click="inputType='noteTypes.image'">img</button>
-                <button class="note-type-option-btn note-type-video-btn" @click="inputType='noteTypes.video'">video</button>
-                <button class="note-type-option-btn note-type-audio-btn" @click="inputType='noteTypes.audio'">audio</button>
-                <button class="note-type-option-btn note-type-todos-btn" @click="inputType='noteTypes.todoList'">todos</button>
-                <button class="note-type-option-btn note-type-map-btn" @click="inputType='noteTypes.map'">map</button>
+                <button class="note-type-option-btn note-type-txt-btn" @click="inputType=noteTypes.text">text</button>
+                <button class="note-type-option-btn note-type-img-btn" @click="inputType=noteTypes.image">img</button>
+                <button class="note-type-option-btn note-type-video-btn" @click="inputType=noteTypes.video">video</button>
+                <button class="note-type-option-btn note-type-audio-btn" @click="inputType=noteTypes.audio">audio</button>
+                <button class="note-type-option-btn note-type-todos-btn" @click="inputType=noteTypes.todoList">todos</button>
+                <button class="note-type-option-btn note-type-map-btn" @click="inputType=noteTypes.map">map</button>
             </section>
         </section>
     `,
     data() {
         return {
+            noteTypes: keepService.NOTE_TYPES,
             inputType: null,
             input: null
         }
     },
     created() {
-        this.inputType = noteTypes.text;
+        this.inputType = this.noteTypes.text;
     },
     computed: {
         inputPlaceholder() {
             switch (this.inputType) {
-                case noteTypes.text:
+                case this.noteTypes.text:
                     return "what's on your mind...";
-                case noteTypes.image:
+                case this.noteTypes.image:
                     return 'Enter image URL...';
-                case noteTypes.video:
+                case this.noteTypes.video:
                     return 'Enter video URL...';
-                case noteTypes.audio:
+                case this.noteTypes.audio:
                     return 'Enter audio URL...';
-                case noteTypes.todoList:
+                case this.noteTypes.todoList:
                     return 'Enter comma seperated list...';
-                case noteTypes.map:
+                case this.noteTypes.map:
                     return 'Enter location...';
             }
         }
