d3a129e17672cd7c6520e0a68688be15f500a4cc
Emanuel Ben Hamo <embenh@gmail.com>
2020-02-27 10:26:42 +0200

    completed user msg cmp

--- js/apps/keep/cmps/user-msg.cmp.js
+++ js/apps/keep/cmps/user-msg.cmp.js
@@ -2,9 +2,33 @@ import { eventBus } from '../../../services/event-bus-service.js';
 
 export default {
     template: `
-        <section class="user-msg-container">
-            <div class="user-msg"></div>
+        <section class="user-msg-container" v-if="showMsg">
+            <button @click="onCloseUserMessage">X</button>
+            <div class="user-msg">{{msg}}</div>
         </section>
     `,
-    
+    data() {
+        return {
+            msg: null,
+            showMsg: false,
+            displayTime: 2000,
+            timeout: null
+        }
+    },
+    created() {
+        eventBus.$on('showUserMsg', (msg) => {
+            clearTimeout(this.timeout);
+            this.msg = msg.txt;
+            this.showMsg = true;
+            this.timeout = setTimeout(() => {
+                this.showMsg = false;
+            }, this.displayTime);
+        })
+    },
+    methods: {
+        onCloseUserMessage() {
+            clearTimeout(this.timeout);
+            this.showMsg = false;
+        }
+    }
 }
