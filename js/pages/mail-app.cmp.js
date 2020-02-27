8754d13ff0e57157c10173737f40e45dc08fed4e
Emanuel Ben Hamo <embenh@gmail.com>
2020-02-27 14:12:33 +0200

    refcator to mail service

--- js/pages/mail-app.cmp.js
+++ js/pages/mail-app.cmp.js
@@ -4,13 +4,12 @@ import mailList from '../../js/apps/email/cmps/mail-list.cmp.js'
 export default {
     template: `
         <section class="mail-app-container">
+            <router-link to="/">Home</router-link>
             <h1>Welcome to the mail app</h1>
-        <router-link to="/">Home</router-link>
-        <mail-list></mail-list>
-
+            <mail-list></mail-list>
         </section>
     `,
-    components:{
+    components: {
         mailList
     }
 }
