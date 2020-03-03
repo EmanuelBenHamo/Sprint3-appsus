a1615711502c123cec2df695c26f706055ca1004
Rotem <tetch6@gmail.com>
2020-03-03 09:48:45 +0200

    delete fake data

--- js/apps/email/services/mail-fake-data.js
+++ js/apps/email/services/mail-fake-data.js
@@ -82,7 +82,7 @@ export const fakeMails = [
     },
     {
         id: utilService.makeId(utilService.getRandomInt(5, 10)),
-        subject: '1',
+        subject: 'P',
         from:'sent person',
         to: 'reciever person',
         cc: 'person for cc',
@@ -104,112 +104,5 @@ export const fakeMails = [
         sentAt: 8,
         starred: false
     },
-    {
-        id: utilService.makeId(utilService.getRandomInt(5, 10)),
-        subject: '3',
-        from:'sent person',
-        to: 'reciever person',
-        cc: 'person for cc',
-        bcc: 'persion for bcc',
-        body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-        state: MAIL_STATE.unread,
-        sentAt: 9,
-        starred: false
-    },
-    {
-        id: utilService.makeId(utilService.getRandomInt(5, 10)),
-        subject: '4',
-        from:'sent person',
-        to: 'reciever person',
-        cc: 'person for cc',
-        bcc: 'persion for bcc',
-        body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-        state: MAIL_STATE.read,
-        sentAt: 10,
-        starred: false
-    },
-    {
-        id: utilService.makeId(utilService.getRandomInt(5, 10)),
-        subject: '5',
-        from:'sent person',
-        to: 'reciever person',
-        cc: 'person for cc',
-        bcc: 'persion for bcc',
-        body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-        state: MAIL_STATE.unread,
-        sentAt: 11,
-        starred: false
-    },
-    {
-        id: utilService.makeId(utilService.getRandomInt(5, 10)),
-        subject: '5',
-        from:'sent person',
-        to: 'reciever person',
-        cc: 'person for cc',
-        bcc: 'persion for bcc',
-        body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-        state: MAIL_STATE.unread,
-        sentAt: 11,
-        starred: false
-    },
-    {
-        id: utilService.makeId(utilService.getRandomInt(5, 10)),
-        subject: '5',
-        from:'sent person',
-        to: 'reciever person',
-        cc: 'person for cc',
-        bcc: 'persion for bcc',
-        body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-        state: MAIL_STATE.unread,
-        sentAt: 11,
-        starred: false
-    },
-    {
-        id: utilService.makeId(utilService.getRandomInt(5, 10)),
-        subject: '5',
-        from:'sent person',
-        to: 'reciever person',
-        cc: 'person for cc',
-        bcc: 'persion for bcc',
-        body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-        state: MAIL_STATE.unread,
-        sentAt: 11,
-        starred: false
-    },
-    {
-        id: utilService.makeId(utilService.getRandomInt(5, 10)),
-        subject: '5',
-        from:'sent person',
-        to: 'reciever person',
-        cc: 'person for cc',
-        bcc: 'persion for bcc',
-        body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-        state: MAIL_STATE.unread,
-        sentAt: 11,
-        starred: false
-    },
-    {
-        id: utilService.makeId(utilService.getRandomInt(5, 10)),
-        subject: '5',
-        from:'sent person',
-        to: 'reciever person',
-        cc: 'person for cc',
-        bcc: 'persion for bcc',
-        body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-        state: MAIL_STATE.unread,
-        sentAt: 11,
-        starred: false
-    },
-    {
-        id: utilService.makeId(utilService.getRandomInt(5, 10)),
-        subject: '5',
-        from:'sent person',
-        to: 'reciever person',
-        cc: 'person for cc',
-        bcc: 'persion for bcc',
-        body: 'ipsum dolor sit amet..", comes from a line in section 1.10.32.ipsum dolor sit amet..", comes from a line in section 1.10.32.ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-        state: MAIL_STATE.unread,
-        sentAt: 11,
-        starred: false
-    },
+    
 ]
